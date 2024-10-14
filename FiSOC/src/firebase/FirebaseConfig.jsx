3067fb2212f16a427f767daa8d90684e21b310bd
Tejaswini2004313 <tejaswinipatkar55@gmail.com>
2024-10-14 18:34:31 +0530

    changes done

--- FiSOC/src/firebase/FirebaseConfig.jsx
+++ FiSOC/src/firebase/FirebaseConfig.jsx
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 //web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyA8qDtcw9U6mNl239ylnD1vjZRGJx3TdG8",
-  authDomain: "fisoc-93d84.firebaseapp.com",
-  projectId: "fisoc-93d84",
-  storageBucket: "fisoc-93d84.appspot.com",
-  messagingSenderId: "551476230851",
-  appId: "1:551476230851:web:22de361093377ff1028076",
+  apiKey: "your-api-key",
+  authDomain: "your-auth-domain",
+  projectId: "your-project-id",
+  storageBucket: "your-storage-bucket",
+  messagingSenderId: "your-messaging-sender-id",
+  appId: "your-app-id",
 };
 
 // Initialize Firebase

a5e475c75fbedf3efb1a23fc56976ddb2da3ffef
Tejaswini2004313 <tejaswinipatkar55@gmail.com>
2024-10-14 18:52:45 +0530

    another changes

--- FiSOC/src/firebase/FirebaseConfig.jsx
+++ FiSOC/src/firebase/FirebaseConfig.jsx
@@ -5,12 +5,12 @@ import { getStorage } from "firebase/storage";
 
 //web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "your-api-key",
-  authDomain: "your-auth-domain",
-  projectId: "your-project-id",
-  storageBucket: "your-storage-bucket",
-  messagingSenderId: "your-messaging-sender-id",
-  appId: "your-app-id",
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_FIREBASE_APP_ID,
 };
 
 // Initialize Firebase
